272627b160aea2ba44e942b0a6cc9ce4d1cacf07
Mohammed Alhashmy <msh.alhashmy@gmail.com>
2026-08-01 14:44:53 +0300

    Databases - SQLite Retrieve Data From Database

---
+++ Databases-SQLiteRetrieveDataFromDatabase.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+db = sqlite3.connect("Test.db")
+
+cr = db.cursor()
+
+cr.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER, name TEXT)")
+
+# cr.execute("insert into users(user_id, name) values(1, 'Mohammed')")
+# cr.execute("insert into users(user_id, name) values(2, 'alex')")
+# cr.execute("insert into users(user_id, name) values(3, 'dune')")
+
+cr.execute("select user_id, name from users")
+
+# print(cr.fetchone())
+# print(cr.fetchone())
+# print(cr.fetchone())
+# print(cr.fetchone())
+#print(cr.fetchall())
+print(cr.fetchmany(2))
+
+
+db.commit()
