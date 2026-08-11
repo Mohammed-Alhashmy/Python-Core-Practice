41794462654e2cfcec38bbb73b80b400559ca05c
Mohammed Alhashmy <msh.alhashmy@gmail.com>
2026-08-11 19:51:53 +0300

    Unit Testing With Unittest

---
+++ UnitTestingWithUnittest.py
@@ -0,0 +1,48 @@
+import unittest
+
+
+assert 9 * 6 == 54, "nah its 54"
+
+def test_one():
+
+    assert 5 * 5 == 25, "must be 25"
+
+def test_two():
+
+    assert 5 * 4 == 20, "must be 20"
+
+
+if __name__ == "__main__":
+
+    test_one()
+    test_two()
+
+    print("all the test good ")
+
+
+
+
+
+
+class MyTestClass(unittest.TestCase):
+
+    def test_one(self):
+
+        self.assertTrue(100 > 50 , "Must True")
+
+    def test_two(self):
+
+        self.assertEqual(5 * 5, 25, "Should be 25")
+
+    def test_three(self):
+
+        self.assertGreater(100, 22, "Must be True")
+
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+
+
+
