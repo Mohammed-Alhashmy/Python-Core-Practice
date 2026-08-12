0c6c9d37cc45111b1d35472f846cbdcd619293a9
Mohammed Alhashmy <msh.alhashmy@gmail.com>
2026-08-12 18:57:04 +0300

    Flask - Create And Extends HTML Templates

--- Flask_add_first_page.py
+++ Flask_add_first_page.py
@@ -14,5 +14,11 @@ def about():
 
     return render_template("aboutpage.html", pt="About-Page")
 
+@my_skill.route("/settings")
+def settings():
+
+    return render_template("settings.html", pt="settings-Page")
+
+
 if __name__ == "__main__":
     my_skill.run(debug=True)
