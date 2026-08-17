18b4784a3b2126e3b9c4771e77f91076d5a14e36
Mohammed Alhashmy <msh.alhashmy@gmail.com>
2026-08-17 12:01:06 +0300

    Flask - Skills Page Using List Data

--- Flask_add_first_page.py
+++ Flask_add_first_page.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 
+mylist=[("Python", 70), ("Html", 40), ("CSS", 23)]
+
+
 my_skill = Flask(__name__)
-
-
 
 @my_skill.route("/")
 def homepage():
@@ -14,10 +15,22 @@ def about():
 
     return render_template("aboutpage.html", pt="About-Page", )
 
-@my_skill.route("/myskill")
-def myskill():
+@my_skill.route("/add")
+def add():
 
-    return render_template("Test_myskill.html", pt="myskill-page", custom_css="myskill")
+    return render_template("Test_myskill.html", 
+                           pt="myskill-page", 
+                           custom_css="myskill")
+
+@my_skill.route("/myskills")
+def addskills():
+
+    return render_template("myskills.html", 
+                           pt="Skills-Page",
+                           custom_css1="skill_page",
+                           page_head="My Skills", 
+                           description="Here is my skills page ",
+                           myData=mylist)
 
 
 if __name__ == "__main__":
